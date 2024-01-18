ff08ab2d847aa3f86a943ad71c2156f7ed5bf3f5
Wiktor <wczetyrbok@griddynamics.com>
2024-01-18 13:56:03 +0100

    add initial files

---
+++ src/main/java/com/chess/together/leaderboard/LeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.chess.together.leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderboardApplication.class, args);
+	}
+
+}
